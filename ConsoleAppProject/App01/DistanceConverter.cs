1a5ada81dd9584fa2116dd6fcb3872f10c0d68db
es9dpw <es9dpw999@gmail.com>
2023-02-09 10:25:45 +0000

    commit

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,13 +10,43 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        const int mTOf = 5280;
+        int choice = 0;
+        double miles = 0;
+        double feet = 0;
+
         public void Run()
         {
+            Menu();
+            if (choice == 1){
+                MilesToFeet();
+            }
+            else if (choice == 2){
+                FeetToMiles();
+            }
+            else{
+                Menu();
+            }
+        }
+
+        public void Menu()
+        {
+            Console.Write("Enter 1 to convert miles to feet, enter 2 to convert feet to miles");
+            choice = Convert.ToInt32(Console.ReadLine());
+        }
+        public void MilesToFeet()
+        {
             Console.Write("Please enter the number of miles: ");
-            double miles = Convert.ToDouble(Console.ReadLine());
-            const int mTOf = 5280;
-            double feet = miles * mTOf;
+            miles = Convert.ToDouble(Console.ReadLine());
+            feet = miles * mTOf;
             Console.WriteLine("There are " + feet + " feet in " + miles + " miles.");
+        }
+        public void FeetToMiles()
+        {
+            Console.Write("Please enter the number of feet: ");
+            feet = Convert.ToDouble(Console.ReadLine());
+            miles = feet / mTOf;
+            Console.WriteLine("There are " + miles + " feet in " + miles + " feet.");
         }
     }
 }
