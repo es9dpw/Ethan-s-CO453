271e617529b8b4a967e15fa0e88ed6ddafaf9c01
es9dpw <es9dpw999@gmail.com>
2023-02-23 11:09:37 +0000

    commit

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -10,10 +10,11 @@ namespace ConsoleAppProject.App02
     /// </author>
     public class BMI
     {
+        bool exit = false;
+        string units;
         double weightInput;
         double heightInput;
         double bmi;
-        string units;
         public void Run()
         {
             Console.WriteLine("\n =================================================");
@@ -24,13 +25,47 @@ namespace ConsoleAppProject.App02
             Console.WriteLine("           using their height and weight          ");
             Console.WriteLine(" =================================================\n");
 
-            
+            while(exit == false){
+                UnitType();
+
+                if (string.Equals(units, "metric") || string.Equals(units, "imperial")){
+                    InputHeight();
+                    InputWeight();
+                    CalculateBMI();
+                }
+                
+                else if (string.Equals(units, "")){
+                    exit = true;
+                    Console.WriteLine("Exiting Converter...\n");
+                }
+                
+                else{
+                    Console.WriteLine("Invalid option.\n");
+                }
+            }
         }
 
         public void UnitType()
         {
             Console.Write("Enter which unit type you would like to use, 'metric' or 'imperial'. Or enter nothing to exit the converter: ");
             units = Console.ReadLine();
+        }
+
+        public void InputHeight()
+        {
+            Console.Write("Please enter your height: ");
+            heightInput = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void InputWeight()
+        {
+            Console.Write("Please enter your weight: ");
+            weightInput = Convert.ToDouble(Console.ReadLine());
+        }
+
+        public void CalculateBMI()
+        {
+            
         }
     }
 }
