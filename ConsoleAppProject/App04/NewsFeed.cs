f66e2346c2fc51e0838858b2fded25a6fbf6f19f
es9dpw <es9dpw999@gmail.com>
2023-03-23 11:25:08 +0000

    commit

--- ConsoleAppProject/App04/NewsFeed.cs
+++ ConsoleAppProject/App04/NewsFeed.cs
@@ -20,6 +20,59 @@ namespace ConsoleAppProject.App04
     ///</author> 
     public class NewsFeed
     {
+        bool exit = false;
+        bool loggedIn = false;
+        string menuChoice;
+        public int postCount = -1;
+        string user = "";
+
+        public void Run()
+        {
+            Console.WriteLine("\n =================================================");
+            Console.WriteLine("               App04: Social Network              ");
+            Console.WriteLine("                  by Ethan Smith                  ");
+            Console.WriteLine(" =================================================");
+            Console.WriteLine("            This is an app to create             ");
+            Console.WriteLine("         and view text and photo posts           ");
+            Console.WriteLine(" =================================================\n");
+            //creates the heading for the app and displays it to the user;
+
+            while(exit == false){
+                Menu();
+                //calls the Menu method
+
+                if (string.Equals(menuChoice, "1")){
+                //checks if the entred the first menu option
+                    Login();
+                }
+            }
+        }
         
+        public void Menu(){
+            Console.Write("MAIN MENU\n1. log");
+            if(loggedIn == false){
+                Console.Write("in");
+            }
+            else if(loggedIn == true){
+                Console.Write("out");
+            }
+            Console.Write("\n2. Add message post\n3. Add photo post\n4. Display all posts\n5. Display all posts by an author\n6. Exit App\n\nPlease enter your choice: ");
+            menuChoice = Console.ReadLine();
+        }
+        //takes the users input on which menu option they want
+
+        public void Login(){
+            if(loggedIn == false){
+                Console.Write("Enter your username to login: ");
+                user = Console.ReadLine();
+                loggedIn = true;
+                Console.WriteLine(user + " logged in.");
+            }
+            else if(loggedIn == true){
+                Console.Write("logging out...");
+                loggedIn = false;
+                user = "";
+            }
+        }
     }
 }
