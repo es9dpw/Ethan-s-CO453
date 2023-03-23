ce8b7ee0a09467178b91c81684a8a9501752ec74
es9dpw <es9dpw999@gmail.com>
2023-03-23 10:26:31 +0000

    commit

--- ConsoleAppProject/App04/NewsFeed.cs
+++ ConsoleAppProject/App04/NewsFeed.cs
@@ -20,59 +20,6 @@ namespace ConsoleAppProject.App04
     ///</author> 
     public class NewsFeed
     {
-        private readonly List<MessagePost> messages;
-        private readonly List<PhotoPost> photos;
-
-        ///<summary>
-        /// Construct an empty news feed.
-        ///</summary>
-        public NewsFeed()
-        {
-            messages = new List<MessagePost>();
-            photos = new List<PhotoPost>();
-        }
-
-
-        ///<summary>
-        /// Add a text post to the news feed.
-        /// 
-        /// @param text  The text post to be added.
-        ///</summary>
-        public void AddMessagePost(MessagePost message)
-        {
-            messages.Add(message);
-        }
-
-        ///<summary>
-        /// Add a photo post to the news feed.
-        /// 
-        /// @param photo  The photo post to be added.
-        ///</summary>
-        public void AddPhotoPost(PhotoPost photo)
-        {
-            photos.Add(photo);
-        }
-
-        ///<summary>
-        /// Show the news feed. Currently: print the news feed details to the
-        /// terminal. (To do: replace this later with display in web browser.)
-        ///</summary>
-        public void Display()
-        {
-            // display all text posts
-            foreach (MessagePost message in messages)
-            {
-                message.Display();
-                Console.WriteLine();   // empty line between posts
-            }
-
-            // display all photos
-            foreach (PhotoPost photo in photos)
-            {
-                photo.Display();
-                Console.WriteLine();   // empty line between posts
-            }
-        }
+        
     }
-
 }
